ec72661209fc6ca7e9b9bc8bafb2c30e1e531084
antho1404 <anthony.estebe@gmail.com>
2025-03-23 16:20:55 +0700

    add ui component and theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { Providers } from "@/components/providers";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { headers } from "next/headers";
+import Image from "next/image";
+import Link from "next/link";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,8 +31,24 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-muted/50 antialiased`}
       >
+        <div className="absolute inset-0 -z-10 bg-gradient-to-tr from-primary/20 to-accent/20 blur-xl" />
         <Providers cookie={(await headers()).get("cookie") || ""}>
-          {children}
+          <div className="flex min-h-screen flex-col">
+            <main className="flex flex-grow items-center justify-center px-4 sm:px-6 lg:px-8">
+              {children}
+            </main>
+            <footer className="flex items-center justify-center gap-1 py-8 text-center text-sm text-muted-foreground">
+              Powered by{" "}
+              <Link href="https://liteflow.com">
+                <Image
+                  src="/liteflow.svg"
+                  alt="Liteflow Logo"
+                  width={96}
+                  height={24}
+                />
+              </Link>
+            </footer>
+          </div>
         </Providers>
       </body>
     </html>
