34f27cf7d248993ff519ad402ffc1214d9615e5e
antho1404 <anthony.estebe@gmail.com>
2025-03-31 22:23:06 +0700

    disable automatic refetch

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -6,7 +6,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren, useMemo } from "react";
 import { cookieToInitialState, WagmiProvider } from "wagmi";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+    },
+  },
+});
 
 export function Providers({
   cookie,
